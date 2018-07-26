80597365a79f192aacb694d6c7a7e061b002ec21
jose.pereda <jose.pereda@gluonhq.com>
2018-07-26 11:54:14 +0200

    AWS-Java-SDK: Bump version number

--- aws-java-sdk-auth-ui/src/main/java/com/amazonaws/mobile/auth/ui/SignInView.java
+++ aws-java-sdk-auth-ui/src/main/java/com/amazonaws/mobile/auth/ui/SignInView.java
@@ -52,7 +52,7 @@ public class SignInView extends View {
     }
 
     /** String that represents the SDK Version. */
-    private static final String SDK_VERSION = "2.6.19";
+    private static final String SDK_VERSION = "2.6.19-b2";
 
     /** Common Prefix of the namespaces of different SignIn providers. */
     private static final String NAMESPACE_COMMON_PREFIX = "com.amazonaws.mobile.auth";
